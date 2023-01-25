baabf53ef165cd277c4a4548b630514b3cc5c0ad
UserMW <92095014+user2084@users.noreply.github.com>
2023-01-25 17:39:24 +0100

    Adds 25-elems-windowsum check in energy_falls with 100 steps warmup

--- chsimpy/solution.py
+++ chsimpy/solution.py
@@ -44,6 +44,17 @@ class TimeData:
         return self._data[:,3]
 
     def energy_falls(self, it = None):
+        '''Checks if E2 curve really falls and returns True then.
+
+        Always False if 'it<100 or sum(E2[-50:-25]) < sum(E2[-25:])'.
+        Else if 'E2[it] < E2[it-1] && E2[it] > E2[0]' then it returns True.
+        '''
+        if it<100: # dont check energy during first iterations (arbitrary chosen)
+            return False
+        s1 = np.sum(self.E2[-50:-25])
+        s2 = np.sum(self.E2[-25:])
+        if s1 < s2:
+            return False
         return (self.E2[it] < self.E2[it-1]
                 and self.E2[it] > self.E2[0])
 
