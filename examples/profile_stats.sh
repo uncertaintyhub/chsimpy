bb5671b2a532e26bf7cb2f5f7275e4d16b88804c
user2084 <cofa+commit@posteo.de>
2023-01-26 16:37:23 +0100

    note about snakeviz in profile_stats.sh

--- examples/profile_stats.sh
+++ examples/profile_stats.sh
@@ -1,1 +1,3 @@
+# https://pramodkumbhar.com/2019/05/summary-of-python-profiling-tools-part-i/
 python -m cProfile -o cprofile.stats benchmark.py -N 512 -n500 -r1 -w0 --lcg -s
+echo "You can use 'snakeviz cprofile.stats' to view results. There is also a jupyter notebook for profiles."
