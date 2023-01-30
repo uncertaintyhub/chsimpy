4aae04f2b07ca9e581ff6b521e35896a1e056095
user2084 <cofa+commit@posteo.de>
2023-01-30 13:09:02 +0100

    adds sysbench and perf stat -d

--- examples/profile_perf.sh
+++ examples/profile_perf.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 #1 ntmax
 
+# for more details
+# perf stat -d python benchmark.py -N 512 -n"$1" -r1 -w0 --lcg -s
+
 perf stat -e "cycles,instructions,\
     cache-references,cache-misses,branches,branch-misses,task-clock,faults,\
     minor-faults,cs,migrations" python benchmark.py -N 512 -n"$1" -r1 -w0 --lcg -s
+
+# compare with sysbench
+
+perf stat -e "cycles,instructions,\
+    cache-references,cache-misses,branches,branch-misses,task-clock,faults,\
+    minor-faults,cs,migrations" sysbench cpu run
