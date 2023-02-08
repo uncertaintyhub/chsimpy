dee696613692b0fb6e21aab937ebb4a56324a913
UserMW <92095014+user2084@users.noreply.github.com>
2023-02-08 14:55:46 +0100

    version file, __init__ for examples [WIP]

--- chsimpy/__init__.py
+++ chsimpy/__init__.py
@@ -1,9 +1,14 @@
 from .cli_parser import CLIParser
 from .plotview import PlotView
-from .model import Model
-from .controller import Controller
+from .solver import Solver
+from .simulator import Simulator
 from .parameters import Parameters
 from .solution import Solution
 from .timedata import TimeData
 from .utils import A0, A1, eigenvalues, csv_dump_matrix, csv_load_matrix, get_current_localtime, yaml_load
 from .mport import gradient, dct2, idct2, rem, matlab_lcg_sample
+
+# TODO: implement automatism
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
---
+++ examples/__init__.py
@@ -0,0 +1,10 @@
+import pathlib
+import sys
+
+try:
+    import chsimpy
+except ImportError:
+    _parentdir = pathlib.Path("./").resolve().parent
+    sys.path.insert(0, str(_parentdir))
+    import chsimpy
+    #sys.path.remove(str(_parentdir))
