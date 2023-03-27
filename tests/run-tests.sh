0ddd195d7c8ff9c34d8fccc1a3ff848557e1b97a
UserMW <92095014+user2084@users.noreply.github.com>
2023-03-27 12:47:23 +0200

    adds bash script for running common test cases

---
+++ tests/run-tests.sh
@@ -0,0 +1,17 @@
+testid=`echo $RANDOM | md5sum | head -c 20`
+echo "Running tests (test-ID=$testid)..."
+echo "- GUI tests: you must close any plot windows to continue testing."
+echo
+python -m unittest test.py || exit -1
+python -m unittest test_solution.py || exit -1
+cd ../examples/
+python benchmark.py -N 100 -R 1 -w 0 || exit -1
+python . -n 10 || exit -1
+python . -n 100 --no-gui || exit -1
+python . -n 100 --no-diagrams --update-every=50 -g 'perlin' || exit -1
+cd ../experiments/
+python paper.py --png --yaml --ntmax 50 -N 512 -s 2023 -R 2 -S -P 4 -C --file-id="$testid" || exit -1
+rm *${testid}*
+
+
+echo "[SUCCESS] All tests have been passed successfully."
