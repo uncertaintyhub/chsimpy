c8a6df37ff15558652b5a43f39fca04ff713310a
UserMW <92095014+user2084@users.noreply.github.com>
2023-04-03 13:33:32 +0200

    adds script to run A0xA1 experiments

---
+++ experiments/run-paper.sh
@@ -0,0 +1,2 @@
+python paper.py --png --yaml -N 512 -s 2023 -R 100 --file-id='test' -S -P 6 -C
+python paper.py --png --yaml -N 512 -s 2023 -R 100 --file-id='test-independent' -S -P 6 -C --independent
