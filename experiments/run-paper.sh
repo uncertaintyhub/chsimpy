98fc52bfd924cedd07f6af7920206e16599d4469
UserMW <92095014+user2084@users.noreply.github.com>
2023-04-18 11:57:46 +0200

    run script with better CLI arg handling and grid option

--- experiments/run-paper.sh
+++ experiments/run-paper.sh
@@ -1,12 +1,19 @@
 procs=6
+ogrid='-grid'
+runs=25
+
 c0=0.875
-fname="$c0"
-fname_ind="$c0-independent"
-python paper.py --png --yaml -N 512 -s 2023 -R 100 --file-id="${fname}" -S -P $procs --threshold=${c0} --cinit=${c0} > "${fname}.txt"
-python paper.py --png --yaml -N 512 -s 2023 -R 100 --file-id="${fname_ind}" -S -P $procs --independent --threshold=${c0} --cinit=${c0} > "${fname_ind}.txt"
+fname="$c0$ogrid"
+fname_ind="$fname-independent"
+options="--png --yaml -N 512 -s 2023 -R $runs -S -P $procs --threshold=${c0} --cinit=${c0}"
+options="$options --A-grid"
+python paper.py $options --file-id="${fname}"  > "${fname}.txt"
+python paper.py $options --file-id="${fname_ind}" --independent > "${fname_ind}.txt"
 
 c0=0.89
-fname="$c0"
-fname_ind="$c0-independent"
-python paper.py --png --yaml -N 512 -s 2023 -R 100 --file-id="${fname}" -S -P $procs --threshold=${c0} --cinit=${c0} > "${fname}.txt"
-python paper.py --png --yaml -N 512 -s 2023 -R 100 --file-id="${fname_ind}" -S -P $procs --independent --threshold=${c0} --cinit=${c0} > "${fname_ind}.txt"
+fname="$c0$ogrid"
+fname_ind="$fname-independent"
+options="--png --yaml -N 512 -s 2023 -R $runs -S -P $procs --threshold=${c0} --cinit=${c0}"
+options="$options --A-grid"
+python paper.py $options --file-id="${fname}" > "${fname}.txt"
+python paper.py $options --file-id="${fname_ind}" --independent > "${fname_ind}.txt"
