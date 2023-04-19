09c5b625ec200128dddde98756dd3c40e24c756b
UserMW <92095014+user2084@users.noreply.github.com>
2023-04-19 15:55:28 +0200

    removes unneeded code

--- chsimpy/mport.py
+++ chsimpy/mport.py
@@ -3,31 +3,6 @@ Port of MATLAB functions
 """
 
 import numpy as np
-#from scipy.fftpack import dct, idct
-from scipy.fftpack import dctn, idctn
-
-
-def gradient(f, d):
-    """Computes np.gradient on f with uniform spacing d along axis [0, 1] with edge_order=1 like matlab does
-
-    :param f: matrix or n-dimensional array
-    :param d: uniform spacing
-    :return: gradient of f
-    """
-    return np.gradient(f, d, axis=[0, 1], edge_order=1)
-
-
-def dct2(block):
-    return dctn(block, norm="ortho")
-
-
-def idct2(block):
-    return idctn(block, norm="ortho")
-
-
-def rem(a, b):
-    """np.remainder of division, like matlab rem()"""
-    return np.remainder(a, b)
 
 
 def _lcg(x, a, c, m):
