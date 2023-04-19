a328b4398e28503419dd98c32f0adfaa76cfc5cd
UserMW <92095014+user2084@users.noreply.github.com>
2023-04-19 15:56:46 +0200

    run-tests runs fix (experiment.py)

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -1,6 +1,6 @@
 testid=`echo $RANDOM | md5sum | head -c 20`
 echo "Running tests (test-ID=$testid)..."
-echo "- GUI tests: you must close any plot windows to continue testing."
+echo "- GUI tests: you must CLOSE any plot windows to CONTINUE testing."
 echo
 python -m unittest test.py || exit -1
 python -m unittest test_solution.py || exit -1
@@ -10,7 +10,7 @@ python . -n 10 || exit -1
 python . -n 100 --no-gui || exit -1
 python . -n 100 --no-diagrams --update-every=50 -g 'perlin' || exit -1
 cd ../experiments/
-python paper.py --png --yaml --ntmax 50 -N 512 -s 2023 -R 2 -S -P 4 -C --file-id="$testid" || exit -1
+python experiment.py --png --yaml --ntmax 50 -N 512 -s 2023 -R 2 -S -P 4 --file-id="$testid" || exit -1
 rm *${testid}*
 
 
