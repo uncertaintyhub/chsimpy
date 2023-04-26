0657e440330c60c80f9ef743d075cdb64b495ef9
UserMW <92095014+user2084@users.noreply.github.com>
2023-04-26 13:32:10 +0200

    adds loops for more experiments (seeds, A, c0), U no more exported

--- experiments/run-experiment.sh
+++ experiments/run-experiment.sh
@@ -1,19 +1,22 @@
 procs=6
-ogrid='-grid'
 runs=100
+exportcsv='E,E2,SA'
 
-c0=0.875
-fname="$c0$ogrid"
-fname_ind="$fname-independent"
-options="--png --yaml -N 512 -s 2023 -R $runs -S -P $procs --threshold=${c0} --cinit=${c0}"
-options="$options --A-grid"
-python experiment.py $options --file-id="${fname}"  > "${fname}.txt"
-python experiment.py $options --file-id="${fname_ind}" --independent > "${fname_ind}.txt"
+Asrcs='uniform sobol grid'
+declare -a seeds=(2023 89513)
+declare -a c0s=(0.89)
 
-c0=0.89
-fname="$c0$ogrid"
-fname_ind="$fname-independent"
-options="--png --yaml -N 512 -s 2023 -R $runs -S -P $procs --threshold=${c0} --cinit=${c0}"
-options="$options --A-grid"
-python experiment.py $options --file-id="${fname}" > "${fname}.txt"
-python experiment.py $options --file-id="${fname_ind}" --independent > "${fname_ind}.txt"
+for Asrc in $Asrcs; do
+  for seed in "${seeds[@]}"; do
+    for c0 in "${c0s[@]}"; do
+      fname="$c0-$Asrc-$seed"
+      fname_ind="$fname-independent"
+      options="--png --yaml -N 512 -s $seed -R $runs --A-source=$Asrc -S -P $procs --threshold=${c0} --cinit=${c0}"
+      options="$options --export-csv=$exportcsv -C"
+      # A0xA1
+      python experiment.py $options --file-id="${fname}"  > "${fname}.txt"
+      # A0+A1
+      python experiment.py $options --file-id="${fname_ind}" --independent > "${fname_ind}.txt"
+    done
+  done
+done
