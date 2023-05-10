1efc816f4ec031464958971f0d8a077deea4cf7b
UserMW <92095014+user2084@users.noreply.github.com>
2023-05-10 11:08:38 +0200

    restructured main with main() method for CLI app install

--- chsimpy/__main__.py
+++ chsimpy/__main__.py
@@ -1,35 +1,29 @@
 #!/usr/bin/env python
-# TODO: provide a __main__.py for chsimpy (https://docs.python.org/3/library/__main__.html)
 
-import pathlib
-import sys
-
-try:
-    import chsimpy
-except ImportError:
-    _parentdir = pathlib.Path("./").resolve().parent
-    sys.path.insert(0, str(_parentdir))
-    import chsimpy
-    # sys.path.remove(str(_parentdir))
-
-from chsimpy import CLIParser, Simulator, utils
+from . import utils
+from .cli_parser import CLIParser
+from .simulator import Simulator
 
 
-if __name__ == '__main__':
+def main():
     parser = CLIParser()
     parser.print_info()
     params = parser.get_parameters()
-    simulator = chsimpy.simulator.Simulator(params)
+    simulator = Simulator(params)
     print(str(params).replace(", '", "\n '"))
 
     solution = simulator.solve()
     simulator.render()
     simulator.export()
     print(f"computed_steps = {solution.computed_steps}, "
-          f"t0 = {solution.t0:g} s ({chsimpy.utils.sec_to_min_if(solution.t0)}), "
+          f"t0 = {solution.t0:g} s ({utils.sec_to_min_if(solution.t0)}), "
           f"stop reason = {solution.stop_reason}")
     if simulator.export_requested():
         print(f"File ID = {simulator.solution_file_id}")
     if simulator.gui_requested():
         simulator.view.show(block=True)
     parser.parser.exit()
+
+
+if __name__ == '__main__':
+    main()
