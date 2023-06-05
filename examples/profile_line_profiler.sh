792ef059ce60ba19277a5d6680b92ace99d3a9b9
UserMW <92095014+user2084@users.noreply.github.com>
2023-06-05 10:29:59 +0200

    removes profile bash scripts

--- examples/profile_line_profiler.sh
+++
@@ -1,5 +0,0 @@
-# https://github.com/rkern/line_profiler
-# pip install line_profiler
-# decorate a function with @profile
-kernprof -l benchmark.py -N 512 -n500 -r1 -w0 --lcg -s
-echo "You can 'python -m line_profiler benchmark.py.lprof' to view results."
--- examples/profile_mprof.sh
+++
@@ -1,3 +0,0 @@
-# Adding the profile decorator to a function(ensure no from memory_profiler import profile statement)
-mprof run --python python benchmark.py -r1 -w0 --lcg -s
-#mprof plot
