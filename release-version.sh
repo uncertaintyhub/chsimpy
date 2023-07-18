730fc2d231a54b15bc9e6b63ed75db657275f51d
UserMW <92095014+user2084@users.noreply.github.com>
2023-07-18 11:36:22 +0200

    do separate commit for version bumping

--- release-version.sh
+++ release-version.sh
@@ -12,7 +12,7 @@ grep -E "__version__ = '[0-9]+\.[0-9]+\.[0-9]+'" chsimpy/version.py || exit 1
 sed -Ei "s/version='[0-9]+\.[0-9]+\.[0-9]+',/version='$tag',/" setup.py
 git add chsimpy/version.py || exit 1
 git add setup.py || exit 1
-git commit --amend --no-edit || exit 1
+git commit -m "bumps version to ${tag}" || exit 1
 git tag -a ${tag} -m "Release version ${tag}" || exit 1
 curr_branch=`git rev-parse --abbrev-ref HEAD`
 if [ "${curr_branch}" != "main" ]; then
