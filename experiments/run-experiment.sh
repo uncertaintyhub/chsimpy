1c14a43e048846c2c7f6f4d49ed5b91da3cd36d2
UserMW <92095014+user2084@users.noreply.github.com>
2023-07-23 14:58:22 +0200

    run script with args for cores and number of runs

--- experiments/run-experiment.sh
+++ experiments/run-experiment.sh
@@ -7,21 +7,23 @@ then
 else
   chsimpy='chsimpy-experiment'
 fi
-procs=6
-runs=100
+
+runs=$1
+procs=$2  # number of cpu processes
 exportcsv='E,E2,SA'
 
-Asrcs='uniform sobol grid'
-declare -a seeds=(2023 89513)
+Asrcs='uniform'
+declare -a seeds=(2023)
 declare -a c0s=(0.89)
+kappabase=0.0314434000476531  # TODO: auto detect via chsimpy
 
 for Asrc in $Asrcs; do
   for seed in "${seeds[@]}"; do
     for c0 in "${c0s[@]}"; do
       fname="$c0-$Asrc-$seed"
       fname_ind="$fname-independent"
-      options="--png --yaml -N 512 -s $seed -R $runs --A-source=$Asrc -S -P $procs --threshold=${c0} --cinit=${c0}"
-      options="$options --export-csv=$exportcsv -C"
+      options="--png --yaml -N 512 -s ${seed} -R ${runs} --A-source=${Asrc} -S -P ${procs} --threshold=${c0} --cinit=${c0}"
+      options="$options -K ${kappabase} --export-csv=${exportcsv} -C"
       # A0xA1
       $chsimpy $options --file-id="${fname}"  > "${fname}.txt"
       # A0+A1
