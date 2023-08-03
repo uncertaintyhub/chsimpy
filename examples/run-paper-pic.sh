2ad997825d4abbdc0e2a662833edf52ed054df46
UserMW <92095014+user2084@users.noreply.github.com>
2023-08-03 15:27:27 +0200

    removes -K CLI arg in script

--- examples/run-paper-pic.sh
+++ examples/run-paper-pic.sh
@@ -11,5 +11,5 @@ fi
 c0=0.875
 for t in 1 60 320 1020; do
   echo "$t min"
-  $chsimpy --cinit=$c0 --threshold=$c0 -K 0.0314434000476531 -t $t -z --no-diagrams --png --yaml --export-csv='E2,E,U,SA' --file-id="paper-pic-${t}min-$c0" --no-gui
+  $chsimpy --cinit=$c0 --threshold=$c0 -t $t -z --no-diagrams --png --yaml --export-csv='E2,E,U,SA' --file-id="paper-pic-${t}min-$c0" --no-gui
 done
